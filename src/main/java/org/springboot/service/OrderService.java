794334a5e5878b7b979be91b7cf2fe0460853f64
DanielSan2023 <electronic.san@gmail.com>
2025-03-30 18:23:36 +0200

    added method for create and save order in OrderService class added method for get order by Id with products List added method for get All orders

--- src/main/java/org/springboot/service/OrderService.java
+++ src/main/java/org/springboot/service/OrderService.java
@@ -1,11 +1,20 @@
 package org.springboot.service;
 
+import org.springboot.dto.OrderDto;
+import org.springboot.exception.ProductNotFoundException;
 import org.springboot.model.Order;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public interface OrderService {
-    public Order addOrder(String customerId, List<String> productEans);
+    Order addOrder(String customerId, List<String> productEans) throws ProductNotFoundException;
+
+    Iterable<Order> getAllOrders() throws IOException;
+
+    Order getById(String id) throws IOException;
+
+     OrderDto getOrderWithProducts(String orderId);
 }
--- src/main/java/org/springboot/service/OrderServiceImpl.java
+++ src/main/java/org/springboot/service/OrderServiceImpl.java
@@ -1,8 +1,9 @@
 package org.springboot.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch.core.IndexRequest;
-import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.*;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import org.springboot.dto.OrderDto;
 import org.springboot.exception.ProductNotFoundException;
 import org.springboot.generator.MyUuidGenerator;
 import org.springboot.model.Order;
@@ -10,9 +11,10 @@ import org.springboot.model.Product;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -27,28 +29,86 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order addOrder(String customerId, List<String> productEans) {
-        String orderId = MyUuidGenerator.generateUuid();
-        List<Product> products = new ArrayList<>();
-
         try {
-            for (String ean : productEans) {
-                Product product = productService.getProductByEAN(ean);
-                if (product != null) {
-                    products.add(product);
-                }
-            }
+            double totalAmount = productEans.stream()
+                    .map(ean -> {
+                        try {
+                            return productService.soldProduct(ean);
+                        } catch (ProductNotFoundException e) {
+                            throw new RuntimeException("Product with EAN " + ean + " not found", e);
+                        }
+                    })
+                    .mapToDouble(Product::getPrice)
+                    .sum();
 
-            double totalAmount = products.stream().mapToDouble(Product::getPrice).sum();
-            Order order = new Order(orderId, customerId, Instant.now(), totalAmount, products);
+            String orderId = MyUuidGenerator.generateUuid();
+            Order order = new Order(orderId, customerId, totalAmount, productEans);
 
             saveOrder(order);
-
             return order;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error while processing the order for customerId: " + customerId, e);
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected error occurred while processing the order", e);
+        }
+    }
+
+    @Override
+    public Iterable<Order> getAllOrders() {
+        SearchRequest request = new SearchRequest.Builder()
+                .index("orders-002")
+                .size(100)
+                .build();
+        try {
+            SearchResponse response = client.search(request, Order.class);
+            System.out.println(response);
+
+            List<Hit<Order>> hits = response.hits().hits();
+            List<Order> orders = new ArrayList<>();
+            for (Hit<Order> hit : hits) {
+                Order order = hit.source();
+                orders.add(order);
+            }
+            return orders;
         } catch (IOException e) {
             throw new RuntimeException("Error while processing the order: " + e.getMessage(), e);
-        } catch (ProductNotFoundException e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public Order getById(String id) {
+        GetRequest request = new GetRequest.Builder()
+                .index("orders-002")
+                .id(id)
+                .build();
+
+        try {
+            GetResponse<Order> response = client.get(request, Order.class);
+            if (response.found()) {
+                return response.source();
+            } else {
+                throw new RuntimeException("Order with ID: " + id + " not found");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Order with find product with ID: " + id, e);
+        }
+    }
+
+    public OrderDto getOrderWithProducts(String orderId) {
+        Order order = getById(orderId);
+
+        List<Product> products = order.getProductEans().stream()
+                .map(ean -> {
+                    try {
+                        return productService.soldProduct(ean);
+                    } catch (ProductNotFoundException e) {
+                        throw new RuntimeException("Product with EAN " + ean + " not found", e);
+                    }
+                })
+                .collect(Collectors.toList());
+
+        return new OrderDto(order,products);
     }
 
     private void saveOrder(Order order) throws IOException {
